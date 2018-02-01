0015bf40728f1222ad0218561197d757683c7ca1
sjgosai <sgosai@g.harvard.edu>
2018-01-31 22:31:17 -0500

    updated MCNN

--- HW1/cnn.py
+++ HW1/cnn.py
@@ -113,24 +113,37 @@ elif net_flag == 'multi':
             else:
                 self.s_embeddings.weight.data.copy_(word2vec)
                 self.s_embeddings.weight.requires_grad = False
-            self.conv = nn.Conv2d(2,n_featmaps,kernel_size=(filter_window,300))
-            self.maxpool = nn.AdaptiveMaxPool1d(1)
-            self.linear = nn.Linear(n_featmaps, 2)
+            self.conv = nn.Conv2d(n_featmaps*3,n_featmaps2,kernel_size=(filter_window,1),padding=(1,0))
+            self.conv3 = nn.Conv2d(300,n_featmaps,kernel_size=(3,2),stride=(1,2),padding=(1,0))
+            self.conv5 = nn.Conv2d(300,n_featmaps,kernel_size=(5,2),stride=(1,2),padding=(2,0))
+            self.conv7 = nn.Conv2d(300,n_featmaps,kernel_size=(7,2),stride=(1,2),padding=(3,0))
+            self.avgpool = nn.AvgPool2d(kernel_size=(5,2),stride=(3,2),padding=(2,0))
+            self.maxpool = nn.AdaptiveMaxPool2d(1,2)
+            self.linear = nn.Linear(n_featmaps*3*2, 2)
             self.dropout = nn.Dropout(dropout_rate)
+    #
         def forward(self, inputs): # inputs (bs,words/sentence) 10,7
             bsz = inputs.size(0) # batch size might change
             if inputs.size(1) < 3: # padding issues on really short sentences
-                pads = Variable(torch.zeros(bsz,3-inputs.size(1)))
-                inputs = torch.cat([inputs,pads],dim=1)
-            embeds = self.embeddings(inputs) # 10,7,300
-            embeds = embeds.unsqueeze(1) # 10,1,7,300
-            s_embeds = self.s_embeddings(inputs) # 10,7,300
-            s_embeds = s_embeds.unsqueeze(1) # 10,1,7,300
-            out = torch.cat([embeds,s_embeds],dim=1) # 10,2,7,300
-            out = F.relu(self.conv(out)) # 10,100,6,1
-            out = out.view(bsz,n_featmaps,-1) # 10,100,6
-            out = self.maxpool(out) # 10,100,1
-            out = out.view(bsz,-1) # 10,100
+                pads = Variable(torch.zeros(bsz,3-inputs.size(1))).type(torch.LongTensor)
+                inputs = torch.cat([inputs,pads.cuda()],dim=1)
+            embeds = self.embeddings(inputs) # 10,h,300
+            embeds = embeds.unsqueeze(3)
+            embeds = embeds.permute(0,2,1,3)
+            s_embeds = self.s_embeddings(inputs)
+            s_embeds = s_embeds.unsqueeze(3)
+            s_embeds = s_embeds.permute(0,2,1,3)
+            out = torch.cat([embeds,s_embeds],dim=3)
+            fw3 = self.conv3(out) # 10,100,h,1
+            fw5 = self.conv5(out) # 10,100,h,1
+            fw7 = self.conv7(out) # 10,100,h,1
+            out = torch.cat([fw3,fw5,fw7],dim=1)
+            out = F.relu(out) # 10,300,h/3,1
+            #out = self.avgpool(out)
+            #out = F.relu(self.conv(out))
+            out = out.view(bsz,n_featmaps*3,-1) # 10,300,7
+            out = self.maxpool(out) # 10,300,2
+            out = out.view(bsz,-1) # 10,600   
             out = self.linear(out) # 10,2
             out = self.dropout(out) # 10,2
             return out
