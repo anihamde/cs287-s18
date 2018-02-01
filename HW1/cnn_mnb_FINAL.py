4e44c81b8b58296d39dced71f957248fe8b33d51
Anirudh Suresh <anirudhtx@gmail.com>
2018-01-31 23:39:47 -0500

    tabs fixed

--- HW1/cnn_mnb_FINAL.py
+++ HW1/cnn_mnb_FINAL.py
@@ -202,59 +202,59 @@ model.eval() # lets dropout layer know that this is the test set
 correct = 0
 total = 0
 for batch in val_iter:
-    ys = torch.zeros(batch.text.size(1),1) # mnb outputs
-    for i in range(batch.text.size(1)):
-        x = batch.text.data.numpy()[:,i]
-        y = batch.label.data.numpy()[i]
-        sparse_x = np.zeros(len(TEXT.vocab))
-        for word in x:
-            sparse_x[word] = 1 # += 1
-        ys[i,0] = ((np.dot(r,sparse_x) + b))#>0).astype(float)
-    sentences = batch.text.transpose(1,0)
-    sentences = sentences.cuda()
-    labels = (batch.label==1).type(torch.LongTensor).data
-    labels = labels.cuda()
-    ys = ys.cuda()
-    # change labels from 1,2 to 1,0
-    outputs = model(sentences,Variable(ys))
-    _, predicted = torch.max(outputs.data, 1)
-    total += labels.size(0)
-    correct += (predicted == labels).sum()
+	ys = torch.zeros(batch.text.size(1),1) # mnb outputs
+	for i in range(batch.text.size(1)):
+		x = batch.text.data.numpy()[:,i]
+		y = batch.label.data.numpy()[i]
+		sparse_x = np.zeros(len(TEXT.vocab))
+		for word in x:
+			sparse_x[word] = 1 # += 1
+		ys[i,0] = ((np.dot(r,sparse_x) + b))#>0).astype(float)
+	sentences = batch.text.transpose(1,0)
+	sentences = sentences.cuda()
+	labels = (batch.label==1).type(torch.LongTensor).data
+	labels = labels.cuda()
+	ys = ys.cuda()
+	# change labels from 1,2 to 1,0
+	outputs = model(sentences,Variable(ys))
+	_, predicted = torch.max(outputs.data, 1)
+	total += labels.size(0)
+	correct += (predicted == labels).sum()
 
 print('test accuracy', correct/total)
 
 def test(model):
-    "All models should be able to be run with following command."
-    upload = []
-    # Update: for kaggle the bucket iterator needs to have batch_size 10
-    # test_iter = torchtext.data.BucketIterator(test, train=False, batch_size=10)
-    for batch in test_iter:
-        # Your prediction data here (don't cheat!)
-        ys = torch.zeros(batch.text.size(1),1) # mnb outputs
-        for i in range(batch.text.size(1)):
-            x = batch.text.data.numpy()[:,i]
-            y = batch.label.data.numpy()[i]
-            sparse_x = np.zeros(len(TEXT.vocab))
-            for word in x:
-                sparse_x[word] = 1 # += 1
-            ys[i,0] = ((np.dot(r,sparse_x) + b))#>0).astype(float)
-        sentences = batch.text.transpose(1,0)
-        sentences = sentences.cuda()
-        ys = ys.cuda()
-        probs = model(sentences,Variable(ys))
-        _, argmax = probs.max(1)
-        upload += list(argmax.data)
+	"All models should be able to be run with following command."
+	upload = []
+	# Update: for kaggle the bucket iterator needs to have batch_size 10
+	# test_iter = torchtext.data.BucketIterator(test, train=False, batch_size=10)
+	for batch in test_iter:
+		# Your prediction data here (don't cheat!)
+		ys = torch.zeros(batch.text.size(1),1) # mnb outputs
+		for i in range(batch.text.size(1)):
+			x = batch.text.data.numpy()[:,i]
+			y = batch.label.data.numpy()[i]
+			sparse_x = np.zeros(len(TEXT.vocab))
+			for word in x:
+				sparse_x[word] = 1 # += 1
+			ys[i,0] = ((np.dot(r,sparse_x) + b))#>0).astype(float)
+		sentences = batch.text.transpose(1,0)
+		sentences = sentences.cuda()
+		ys = ys.cuda()
+		probs = model(sentences,Variable(ys))
+		_, argmax = probs.max(1)
+		upload += list(argmax.data)
 
-    with open("../data/cnn_mnb_predictions.csv", "w") as f:
-    	writer = csv.writer(f)
-        writer.writerow(['Id','Cat'])
-        idcntr = 0
-        for u in upload:
-            if u == 0:
-                u = 2
-            writer.writerow([idcntr,u])
-            idcntr += 1
-            # f.write(str(u) + "\n")
+	with open("../data/cnn_mnb_predictions.csv", "w") as f:
+		writer = csv.writer(f)
+		writer.writerow(['Id','Cat'])
+		idcntr = 0
+		for u in upload:
+			if u == 0:
+				u = 2
+			writer.writerow([idcntr,u])
+			idcntr += 1
+			# f.write(str(u) + "\n")
 
 test(model)
 
