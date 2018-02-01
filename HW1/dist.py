e211fb4fc7598f68f4d35c4993c1e075f6c7e1e0
elbertgong <elbertgong@college.harvard.edu>
2018-01-31 20:46:15 -0500

    uncommented my old dist stuff for the histogram

--- HW1/dist.py
+++ HW1/dist.py
@@ -42,40 +42,40 @@ sd = torch.load('../../models/cnn_standard.pkl')
 new = sd['embeddings.weight']
 
 # simcos = F.cosine_similarity(orig,new,dim=1)
-# sim = torch.abs(orig-new).sum(1) # l1 distance
+sim = torch.abs(orig-new).sum(1) # l1 distance
 # sim = ((orig-new)**2).sum(1).sqrt() # l2 distance
 # sim = sim.numpy() # vector of 16286 similarity scores
 
 keywords = ['direct','directs','director','produce','produces','producer','vision','spirit','capture','captures',
     'character','actor','role','plot','style']
-# keysim = []
-# # for i in range(len(keywords)):
-# #     key = TEXT.vocab.stoi[keywords[i]]
-# #     keysim.append(sim[key])
-# #     print(keywords[i],sim[key])
+keysim = []
+for i in range(len(keywords)):
+    key = TEXT.vocab.stoi[keywords[i]]
+    keysim.append(sim[key])
+    print(keywords[i],sim[key])
 
-# plt.hist(sim)
-# plt.hist(keysim*1000) # for scale
-# plt.title('Histogram of distances between embedding vectors pre/post-training')
-# plt.legend(['true vocab','keywords (not2scale)'])
-# plt.show()
+plt.hist(sim.numpy())
+plt.hist(keysim*1000) # for scale
+plt.title('Histogram of distances between embedding vectors pre/post-training')
+plt.legend(['true vocab','keywords (not2scale)'])
+plt.show()
 
 # If we have time, we could add a couple more keywords and do a hypothesis test
 
-keylist = [0 for i in keywords]
+# keylist = [0 for i in keywords]
 
-for i in range(len(keywords)):
-	keylist[i] = TEXT.vocab.stoi[keywords[i]]
+# for i in range(len(keywords)):
+# 	keylist[i] = TEXT.vocab.stoi[keywords[i]]
 
-# print(len(orig[keylist[0]]))
-# print(len(orig[keylist[3]]))
-# print(len(new[keylist[0]]))
+# # print(len(orig[keylist[0]]))
+# # print(len(orig[keylist[3]]))
+# # print(len(new[keylist[0]]))
 
-print(keywords[0],keywords[3],F.cosine_similarity(orig[keylist[0]],orig[keylist[3]],dim=0).numpy()[0], F.cosine_similarity(new[keylist[0]],new[keylist[3]],dim=0).numpy()[0])
-print(keywords[2],keywords[5],F.cosine_similarity(orig[keylist[2]],orig[keylist[5]],dim=0).numpy()[0], F.cosine_similarity(new[keylist[2]],new[keylist[5]],dim=0).numpy()[0])
-print(keywords[6],keywords[7],F.cosine_similarity(orig[keylist[6]],orig[keylist[7]],dim=0).numpy()[0], F.cosine_similarity(new[keylist[6]],new[keylist[7]],dim=0).numpy()[0])
-print(keywords[10],keywords[11],F.cosine_similarity(orig[keylist[10]],orig[keylist[11]],dim=0).numpy()[0], F.cosine_similarity(new[keylist[10]],new[keylist[11]],dim=0).numpy()[0])
-print(keywords[7],keywords[9],F.cosine_similarity(orig[keylist[7]],orig[keylist[9]],dim=0).numpy()[0], F.cosine_similarity(new[keylist[7]],new[keylist[9]],dim=0).numpy()[0])
+# print(keywords[0],keywords[3],F.cosine_similarity(orig[keylist[0]],orig[keylist[3]],dim=0).numpy()[0], F.cosine_similarity(new[keylist[0]],new[keylist[3]],dim=0).numpy()[0])
+# print(keywords[2],keywords[5],F.cosine_similarity(orig[keylist[2]],orig[keylist[5]],dim=0).numpy()[0], F.cosine_similarity(new[keylist[2]],new[keylist[5]],dim=0).numpy()[0])
+# print(keywords[6],keywords[7],F.cosine_similarity(orig[keylist[6]],orig[keylist[7]],dim=0).numpy()[0], F.cosine_similarity(new[keylist[6]],new[keylist[7]],dim=0).numpy()[0])
+# print(keywords[10],keywords[11],F.cosine_similarity(orig[keylist[10]],orig[keylist[11]],dim=0).numpy()[0], F.cosine_similarity(new[keylist[10]],new[keylist[11]],dim=0).numpy()[0])
+# print(keywords[7],keywords[9],F.cosine_similarity(orig[keylist[7]],orig[keylist[9]],dim=0).numpy()[0], F.cosine_similarity(new[keylist[7]],new[keylist[9]],dim=0).numpy()[0])
 
 
 
