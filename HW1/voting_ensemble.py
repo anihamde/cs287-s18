ea2884865a776ddfeadc4c1e084831536e914cfe
sjgosai <sgosai@g.harvard.edu>
2018-02-01 08:00:21 -0500

    Create voting_ensemble.py

---
+++ HW1/voting_ensemble.py
@@ -0,0 +1,8 @@
+import pandas as pd
+df_set = []
+for file in sys.argv[1:]:
+    df_set.append( pd.read_csv(file,index_col=0) )
+
+pred = pd.concat(df_set,axis=1).mode(axis=1)
+pred.columns = ["Cat"]
+pred.to_csv("cnn_ensemble1_predictions.csv",header=True)
