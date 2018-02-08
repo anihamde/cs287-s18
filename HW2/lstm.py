moved nnlm precision fixes to lstm code

HW2/lstm.py
@@ -119,7 +119,10 @@ for i in range(num_epochs):
 
 model.eval()
 correct = total = 0
-precisionmat = 1/(range(1,21))
+precisionmat = []
+
+for i in range(0,20):
+    precisionmat.append(1.0/(1.0+i))
 
 for i in range(0,20):
     precisionmat[i] = sum(precisionmat[i:20])
@@ -136,10 +139,12 @@ for batch in iter(val_iter):
     for j in range(n,sentences.size(1)):
         # precision
         out = out[j]
-        _,indices = torch.sort(out,desc=True)
+        _,indices = torch.sort(out,descending=True)
         indices20 = indices[:,0:20]
-        labels = sentences[j] # bs
-        indicmat = np.where(indices20 - labels == 0)
+        labels = sentences[:,j]
+        labels2 = labels.unsqueeze(0)
+        labels2 = labels2.permute(1,0)
+        indicmat = np.where((indices20 - labels2.expand(labels2.size()[0],20)).data.numpy() == 0)
         for k in range(0,len(indicmat[0])):
             colm = indicmat[1][k]
             precisioncalc += precisionmat[colm]
@@ -149,7 +154,7 @@ for batch in iter(val_iter):
         # plain ol accuracy
         _, predicted = torch.max(out.data, 1)
         total += labels.size(0)
-        correct += (predicted == labels).sum()
+        correct += (predicted.numpy() == labels.data.numpy()).sum()
 
 print('Test Accuracy', correct/total)
 print('Precision',precisioncalc/(20*precisioncntr))