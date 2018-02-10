ce6db058d1e3dbe2a193e867f4dfe7353926f9e3
sjgosai <sgosai@g.harvard.edu>
2018-02-09 20:51:35 -0500

    mvavg loss monitor

--- HW2/lstm.py
+++ HW2/lstm.py
@@ -130,7 +130,7 @@ for i in range(num_epochs):
         losses.append(loss.data[0])
         if ctr % 100 == 0:
             print ('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f' 
-                %(epoch+1, num_epochs, ctr, len(train_iter), sum(losses[-100:])/100.0  ))
+                %(epoch+1, num_epochs, ctr, len(train_iter), sum(losses[-500:])/len(losses[-500:])  ))
         hidden = repackage_hidden(hidden)
 
     # can add a net_flag to these file names. and feel free to change the paths
