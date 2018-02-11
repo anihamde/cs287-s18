655a4801ad8b21c928fc4ca1ab717aa3df7c57cc
elbertgong <elbertgong@college.harvard.edu>
2018-02-11 00:29:31 +0000

    tiny edit for python3 and printing

--- HW2/trigram_looper.sh
+++ HW2/trigram_looper.sh
@@ -6,8 +6,9 @@ for alphab in 0.2 0.5 0.8
 do
 	for alphat in 0.2 0.5 0.8
 	do
+		echo "params are $alphab $alphat"
 		echo "params are $alphab $alphat" >> trigramgrid.txt
-		python trigram.py --alphab $alphab --alphat $alphat >> trigramgrid.txt
+		python3 trigram.py --alphab $alphab --alphat $alphat >> trigramgrid.txt
 	done
 done
 exit 0
