ab7e515d59ad8c236b0635ab326dd467505b3009
sjgosai <sgosai@g.harvard.edu>
2018-02-12 16:59:01 -0500

    slice and stack sentences

--- HW2/nnlm.py
+++ HW2/nnlm.py
@@ -172,12 +172,14 @@ if not args.skip_training:
             if sentences.size(1) < n+1: # make sure sentence length is long enough
                 pads = Variable(torch.ones(sentences.size(0),n+1-sentences.size(1)).mul(padidx)).type(torch.cuda.LongTensor)
                 sentences = torch.cat([pads,sentences],dim=1)
+            sentence_stack = torch.LongTensor()
             for j in range(n,sentences.size(1)):
-                model.zero_grad()
-                out = model(sentences[:,j-n:j])
-                loss = criterion(out,sentences[:,j])
-                loss.backward()
-                optimizer.step()
+                torch.cat([sentence_stack,sentences[:,j-n:j+1]], dim=0)            
+            model.zero_grad()
+            out = model(sentences[:,:n])
+            loss = criterion(out,sentences[:,n])
+            loss.backward()
+            optimizer.step()
             ctr += 1
             if ctr % 100 == 0:
                 print ('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f' 
