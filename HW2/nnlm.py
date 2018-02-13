8cfbfe37cc57e28d7b0edce40dd13a5f02f8631f
elbertgong <elbertgong@college.harvard.edu>
2018-02-12 21:10:10 -0500

    epsilon in the trigram

--- HW2/nnlm.py
+++ HW2/nnlm.py
@@ -66,10 +66,6 @@ TEXT.vocab.load_vectors(vectors=Vectors('wiki.simple.vec', url=url)) # feel free
 print("Word embeddings size ", TEXT.vocab.vectors.size())
 print("REMINDER!!! Did you create ../../models/HW2?????")
 
-# TODO: Is 8000 ppl reasonable? Why are my perplexities so high? What separates us and Bengio???
-# compare to lstm/trigram ppls? print ppls of individual batches? or don't validate on full bptt_len?
-# also, do we have the right idea for precision?
-# the lstm losses are also screwed. are the lstm's sentences at least reasonable?
 # TODO: hidden to 300, then to |V|
 # TODO: mixture of models with interpolated trigram (fixed or learned weights)
 # TODO: bengio's idea, set w to zero
--- HW2/trigram.py
+++ HW2/trigram.py
@@ -10,7 +10,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--batch_size','-bs',type=int,default=10,help='set training batch size. default = 10.')
 parser.add_argument('--alphab','-ab',type=float, default=0.2)
 parser.add_argument('--alphat','-at',type=float, default=0.4)
-parser.add_argument('--epsilon','-e',type=float,default=10**-6)
+parser.add_argument('--epsilon','-e',type=float,default=0.1)
 args = parser.parse_args()
 
 # Hyperparameters
@@ -133,6 +133,7 @@ for batch in iter(val_iter):
             if label in indices:
                 precision += precisionmat[indices.index(label)]
             # cross entropy
+            prob = (1-eps)*out[label]+eps/len(TEXT.vocab)
             crossentropy -= np.log(max(out[label],eps))
             # plain ol accuracy
             total += 1
