63ba6c239bfc2a3fdd11ea1cdc835fb2697fee85
sjgosai <sgosai@g.harvard.edu>
2018-02-13 00:29:07 -0500

    Subbed in nn.GRU

--- HW2/gru.py
+++ HW2/gru.py
@@ -8,10 +8,10 @@ import csv
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description='lstm training runner')
-parser.add_argument('--model_file','-m',type=str,default='../../models/HW2/lstm.pkl',help='Model save target.')
+parser = argparse.ArgumentParser(description='gru training runner')
+parser.add_argument('--model_file','-m',type=str,default='../../models/HW2/gru.pkl',help='Model save target.')
 parser.add_argument('--batch_size','-bs',type=int,default=10,help='set training batch size. default=10.')
-parser.add_argument('--num_layers','-nl',type=int,default=2,help='set number of lstm layers.')
+parser.add_argument('--num_layers','-nl',type=int,default=2,help='set number of gru layers.')
 parser.add_argument('--hidden_size','-hs',type=int,default=500,help='set size of hidden layer.')
 parser.add_argument('--learning_rate','-lr',type=float,default=0.001,help='set learning rate.')
 parser.add_argument('--weight_decay','-wd',type=float,default=0.0,help='set L2 normalization factor.')
@@ -86,12 +86,12 @@ def repackage_hidden(h):
     else:
         return tuple(repackage_hidden(v) for v in h)
 
-class dLSTM(nn.Module):
+class dGRU(nn.Module):
     def __init__(self):
-        super(dLSTM, self).__init__()
+        super(dGRU, self).__init__()
         self.embedding = nn.Embedding(word2vec.size(0),word2vec.size(1),max_norm=emb_mn)
         self.embedding.weight.data.copy_(word2vec)
-        self.lstm = nn.LSTM(word2vec.size(1), hidden_size, n_layers, dropout=dropout_rate)
+        self.gru = nn.GRU(word2vec.size(1), hidden_size, n_layers, dropout=dropout_rate)
         self.linear = nn.Linear(hidden_size, len(TEXT.vocab))
         self.softmax = nn.LogSoftmax(dim=2)
         
@@ -101,7 +101,7 @@ class dLSTM(nn.Module):
         embeds = self.embedding(input) # n,bs,300
         # batch goes along the second dimension
         out = F.dropout(embeds,p=dropout_rate)
-        out, hidden = self.lstm(out, hidden)
+        out, hidden = self.gru(out, hidden)
         out = F.dropout(out,p=dropout_rate)
         # apply the linear and the softmax
         out = self.linear(out) # n,bs,|V|
@@ -110,13 +110,11 @@ class dLSTM(nn.Module):
     
     def initHidden(self):
         h0 = torch.zeros(n_layers, bs, hidden_size).type(torch.FloatTensor)
-        c0 = torch.zeros(n_layers, bs, hidden_size).type(torch.FloatTensor)
         if torch.cuda.is_available():
             h0 = h0.cuda()
-            c0 = c0.cuda()
-        return (Variable(h0), Variable(c0))
+        return Variable(h0)
 
-model = dLSTM()
+model = dGRU()
 if torch.cuda.is_available():
     model.cuda()
     print("CUDA is available, assigning to GPU.", file=sys.stderr)
@@ -190,7 +188,7 @@ if not args.skip_training:
             hidden = repackage_hidden(hidden)
 
         # can add a net_flag to these file names. and feel free to change the paths
-        np.save("../../models/HW2/lstm_losses",np.array(losses))
+        np.save("../../models/HW2/gru_losses",np.array(losses))
         torch.save(model.state_dict(), args.model_file)
         # for early stopping
         acc, prec, ppl = validate()
@@ -200,7 +198,7 @@ else:
 
 
 model.eval()
-with open("lstm_predictions.csv", "w") as f:
+with open("gru_predictions.csv", "w") as f:
     writer = csv.writer(f)
     writer.writerow(['id','word'])
     for i, l in enumerate(open("input.txt"),1):
