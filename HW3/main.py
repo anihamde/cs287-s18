bf898a9ade84bd7a050d8cd9200ab1beb0cf6d43
Anirudh Suresh <anirudhtx@gmail.com>
2018-02-24 00:33:27 -0500

    models

--- HW3/main.py
+++ HW3/main.py
@@ -134,7 +134,7 @@ for epoch in range(n_epochs):
         y_pred = model.predict(x_de, attn_type)
         lesser_of_two_evils = min(y_pred.size(1),x_en.size(1)) # TODO: temporary fix!!
         correct = torch.sum(y_pred[:,1:lesser_of_two_evils]==x_en[:,1:lesser_of_two_evils]) # exclude <s> token in acc calculation
-        avg_acc = 0.95*avg_acc + 0.05*correct[0]/(x_en.size(0)*x_en.size(1))
+        avg_acc = 0.95*avg_acc + 0.05*correct.data[0]/(x_en.size(0)*x_en.size(1))
         (loss + neg_reward).backward()
         torch.nn.utils.clip_grad_norm(model.parameters(), 1) # TODO: is this right? it didn't work last time
         optimizer.step()
