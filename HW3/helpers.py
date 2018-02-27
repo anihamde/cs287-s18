2c734325d163fd2f0cc5d054849a3831e4ecaebd
sjgosai <sgosai@g.harvard.edu>
2018-02-27 01:47:08 -0500

    added repackage hidden

--- HW3/helpers.py
+++ HW3/helpers.py
@@ -30,3 +30,10 @@ def flip(x, dim):
 
 def lstm_hidden(laydir,bs,hiddensz):
     return (Variable(torch.zeros(laydir,bs,hiddensz).cuda()), Variable(torch.zeros(laydir,bs,hiddensz).cuda()))
+
+def repackage_hidden(h):
+    """Wraps hidden states in new Variables, to detach them from their history."""
+    if type(h) == Variable:
+        return Variable(h.data)
+    else:
+        return tuple(repackage_hidden(v) for v in h)
