cf8cb3c169d270367338893fa48b2813816fc08a
sjgosai <sgosai@g.harvard.edu>
2018-02-27 11:48:33 -0500

    added GRU

--- HW3/main.py
+++ HW3/main.py
@@ -131,7 +131,7 @@ If we have time, we can try the pytorch tutorial script with and without attn, t
 How to run jupyter notebooks in cloud?
 Generate longer full sentences with small beams. Not fixed-length.
 ''' 
-from models import AttnNetwork, CandList, S2S
+from models import AttnNetwork, CandList, S2S, AttnGRU
 from helpers import asMinutes, timeSince, escape, flip
 
 if model_type == 0:
