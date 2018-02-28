1cf294fb570f3c01a91076677b90d3f17cb29b11
sjgosai <sgosai@g.harvard.edu>
2018-02-27 19:06:50 -0500

    Updated .forward() output catching

--- HW3/main.py
+++ HW3/main.py
@@ -191,7 +191,7 @@ for epoch in range(n_epochs):
         x_en = batch.trg.transpose(1,0).cuda() # bs,n_en
         if model_type == 1:
             x_de = flip(x_de,1) # reverse direction
-        loss, reinforce_loss, avg_reward, _, _ = model.forward(x_de, x_en)
+        loss, reinforce_loss, avg_reward, _ = model.forward(x_de, x_en)
         print_loss_total -= avg_reward
         plot_loss_total -= avg_reward
 
