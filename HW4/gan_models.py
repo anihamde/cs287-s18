37eaa854e858b69b28c6a1d9a3da96b3fc072fd3
sjgosai <sgosai@g.harvard.edu>
2018-03-30 16:10:41 -0400

    Added new Gen/Dis pair

--- HW4/gan_models.py
+++ HW4/gan_models.py
@@ -75,3 +75,38 @@ class Discriminator1(nn.Module):
         out = F.leaky_relu(self.conv4(out), 0.05) # (?, 256, 1, 1)
         out = out.squeeze()
         return F.sigmoid(self.linear(out))
+
+class Generator2(nn.Module):
+    def __init__(self, latent_dim=32, image_size=28, conv_dim=32):
+        super(Generator2, self).__init__()
+        self.fc = deconv(latent_dim, conv_dim*8, 2, stride=1, pad=0, bn=False)
+        self.deconv1 = deconv(conv_dim*8, conv_dim*4, 4)
+        self.deconv2 = deconv(conv_dim*4, conv_dim*2, 3) # hacky to change kernel size
+        self.deconv3 = deconv(conv_dim*2, conv_dim, 4)
+        self.deconv4 = deconv(conv_dim, 1, 4, bn=False)
+    def forward(self, z):
+        z = z.view(z.size(0), z.size(1), 1, 1)
+        out = self.fc(z) # (?, 256, 2, 2)
+        out = F.leaky_relu(self.deconv1(out), 0.05) # (?, 128, 4, 4)
+        out = F.leaky_relu(self.deconv2(out), 0.05) # (?, 64, 7, 7)
+        out = F.leaky_relu(self.deconv3(out), 0.05) # (?, 32, 14, 14)
+        out = F.sigmoid(self.deconv4(out)) # (?, 1, 28, 28)
+        return out
+
+class Discriminator2(nn.Module):
+    def __init__(self, image_size=28, conv_dim=32):
+        super(Discriminator2, self).__init__()
+        self.conv_dim = conv_dim
+        self.conv1 = conv(1, conv_dim, 4, stride=1, pad=1, bn=False)
+        self.conv2 = conv(conv_dim, conv_dim*2, 4, stride=1, pad=0)
+        self.maxpool= nn.MaxPool2d(2,padding=1)
+        self.linear = nn.Linear(conv_dim*2*6*6, 1024)
+        self.output = nn.Linear(1024, 1)
+    def forward(self, x):
+        out = F.leaky_relu(self.conv1(x), 0.05) # (?, 32, 27, 27)
+        out = self.maxpool(out) # (?, 32, 13, 13)
+        out = F.leaky_relu(self.conv2(out), 0.05) # (?, 64, 10, 10)
+        out = self.maxpool(out) # (?, 64, 6, 6)
+        out = out.view(-1, self.conv_dim*2*6*6) # (?, 64*8*8)
+        out = F.leaky_relu(self.linear(out), 0.05) # (?, 1024)
+        return F.sigmoid(self.output(out).squeeze())
