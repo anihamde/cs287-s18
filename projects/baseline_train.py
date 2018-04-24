20325266ebdabe40d1c8030fdaa24f455d1a3db9
sjgosai <sgosai@g.harvard.edu>
2018-04-24 18:34:31 -0400

    added alpha to RMSprop

--- projects/baseline_train.py
+++ projects/baseline_train.py
@@ -24,6 +24,7 @@ parser.add_argument('--batch_size','-bs',type=int,default=128,help='Batch size')
 parser.add_argument('--num_epochs','-ne',type=int,default=10,help='Number of epochs')
 parser.add_argument('--learning_rate','-lr',type=float,default=0.0001,help='Learning rate')
 parser.add_argument('--rho','-r',type=float,default=0.95,help='rho for Adadelta optimizer')
+parser.add_argument('--alpha','-al',type=float,default=0.98,help='alpha value for RMSprop optimizer')
 parser.add_argument('--weight_decay','-wd',type=float,default=0.0,help='Weight decay constant for optimizer')
 parser.add_argument('--max_weight_norm','-wn',type=float,help='Max L2 norm for weight clippping')
 parser.add_argument('--clip','-c',type=float,help='Max norm for weight clipping')
@@ -100,7 +101,7 @@ if args.optimizer_type == 0:
 elif args.optimizer_type == 1:
     optimizer = torch.optim.Adam(params, lr=args.learning_rate, weight_decay=args.weight_decay)
 elif args.optimizer_type == 2:
-    optimizer = torch.optim.RMSprop(params, lr=args.learning_rate, weight_decay=args.weight_decay)
+    optimizer = torch.optim.RMSprop(params, lr=args.learning_rate, alpha=args.alpha, weight_decay=args.weight_decay)
 
 #criterion = torch.nn.MultiLabelSoftMarginLoss() # Loss function
 criterion = torch.nn.BCEWithLogitsLoss(size_average=False)
