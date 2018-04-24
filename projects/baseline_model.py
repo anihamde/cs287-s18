1bc69c236fe14feb5b5160c3a203adf015afc19f
sjgosai <sgosai@g.harvard.edu>
2018-04-24 15:19:30 -0400

    Added weight_norm clipping

--- projects/baseline_model.py
+++ projects/baseline_model.py
@@ -20,6 +20,42 @@ def to_one_hot(y, n_dims=None):
     y_one_hot = y_one_hot.view(*y.shape, -1)
     return Variable(y_one_hot) if isinstance(y, Variable) else y_one_hot
 
+class Conv1dNorm(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, 
+                 stride=1, padding=0, dilation=1, groups=1, 
+                 bias=True, batch_norm=True, weight_norm=True):
+        super(Conv1dNorm, self).__init__()
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, 
+                              stride, padding, dilation, groups, bias)
+        if weight_norm:
+            self.conv = nn.utils.weight_norm(self.conv)
+        if batch_norm:
+            self.bn_layer = nn.BatchNorm1d(out_channels, eps=1e-05, momentum=0.1, 
+                                           affine=True, track_running_stats=True)
+        
+    def forward(self, input):
+        try:
+            return self.bn_layer( self.conv( input ) )
+        except AttributeError:
+            return self.conv( input )
+        
+class LinearNorm(nn.Module):
+    def __init__(self, in_features, out_features, bias=True, 
+                 batch_norm=True, weight_norm=True):
+        super(LinearNorm, self).__init__()
+        self.linear  = nn.Linear(in_features, out_features, bias=True)
+        if weight_norm:
+            self.linear = nn.utils.weight_norm(self.linear)
+        if batch_norm:
+            self.bn_layer = nn.BatchNorm1d(out_features, eps=1e-05, momentum=0.1, 
+                                           affine=True, track_running_stats=True)
+    def forward(self, input):
+        try:
+            return self.bn_layer( self.linear( input ) )
+        except AttributeError:
+            return self.linear( input )
+        
+        
 class Classic(nn.Module):
     def __init__(self, dropout_prob=0.5):
         super(Classic, self).__init__()
@@ -46,6 +82,43 @@ class Classic(nn.Module):
         out = self.dropout(out)
         return self.output(out) # (?, 164)
 
+class BassetNorm(nn.Module):
+    def __init__(self, dropout_prob=0.3):
+        super(BassetNorm, self).__init__()
+        self.conv1 = Conv1dNorm(4, 300, 19, stride=1, padding=0)
+        self.conv2 = Conv1dNorm(300, 200, 11, stride=1, padding=0)
+        self.conv3 = Conv1dNorm(200, 200, 7, stride=1, padding=0)
+        self.maxpool_4 = nn.MaxPool1d(4,padding=0)
+        self.maxpool_3 = nn.MaxPool1d(3,padding=0)
+        self.linear1 = LinearNorm(200*10, 1000)
+        self.linear2 = LinearNorm(1000, 1000)
+        self.dropout = nn.Dropout(p=dropout_prob)
+        self.output = nn.Linear(1000, 164)
+    def forward(self, x):
+        #if sparse_in: # (?, 600, 4)
+        #    in_seq = to_one_hot(x, n_dims=4).permute(0,3,1,2).squeeze()
+        #else:
+        #    in_seq = x.squeeze()
+        out = F.relu(self.conv1(x)) # (?, 4, 580)
+        out = self.maxpool_3(out) # (?, 30, 145)
+        out = F.relu(self.conv2(out)) # (?, 300, 140)
+        out = self.maxpool_4(out) # (?, 300, 35)
+        out = F.relu(self.conv3(out)) # (?, 500, 32)
+        out = self.maxpool_4(out) # (?, 500, 8)
+        out = out.view(-1, 200*10) # (?, 500*8)
+        out = F.relu(self.linear1(out)) # (?, 800)
+        out = self.dropout(out)
+        out = F.relu(self.linear2(out)) # (?, 800)
+        out = self.dropout(out)
+        return self.output(out) # (?, 164)
+    def clip_norms(self, value):
+        key_chain = [ key for key in self.state_dict().keys() if 'weight_g' in key ]
+        for key in key_chain:
+            module_list = [self]
+            for key_level in key.split('.'):
+                module_list.append( getattr(module_list[-1], key_level) )
+            module_list[-1].data.clamp_(min=0.0,max=value)
+    
 class Basset(nn.Module):
     def __init__(self, dropout_prob=0.3):
         super(Basset, self).__init__()
