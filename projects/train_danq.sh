024ff8c5ac78a83948b84e7ea1ee473b52473314
elbertgong <elbertgong@college.harvard.edu>
2018-04-26 21:55:20 -0400

    tiny ch

--- projects/train_danq.sh
+++ projects/train_danq.sh
@@ -1,18 +1,9 @@
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 0 -lr 0.0001 -wn 7.0 -ne 12 -mf danq_00.pkl -l danq_00.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 0 -lr 0.002 -wn 7.0 -ne 12 -mf danq_01.pkl -l danq_01.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 0 -lr 0.01 -wn 7.0 -ne 12 -mf danq_02.pkl -l danq_02.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 1 -lr 0.0001 -wn 7.0 -ne 12 -mf danq_03.pkl -l danq_03.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 1 -lr 0.002 -wn 7.0 -ne 12 -mf danq_04.pkl -l danq_04.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 1 -lr 0.01 -wn 7.0 -ne 12 -mf danq_05.pkl -l danq_05.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 2 -lr 0.0001 -wn 7.0 -ne 12 -mf danq_06.pkl -l danq_06.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 2 -lr 0.002 -wn 7.0 -ne 12 -mf danq_07.pkl -l danq_07.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 2 -lr 0.01 -wn 10.0 -ne 12 -mf danq_08.pkl -l danq_08.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 0 -lr 0.0001 -wn 10.0 -ne 12 -mf danq_00.pkl -l danq_09.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 0 -lr 0.002 -wn 10.0 -ne 12 -mf danq_01.pkl -l danq_10.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 0 -lr 0.01 -wn 10.0 -ne 12 -mf danq_02.pkl -l danq_11.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 1 -lr 0.0001 -wn 10.0 -ne 12 -mf danq_03.pkl -l danq_12.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 1 -lr 0.002 -wn 10.0 -ne 12 -mf danq_04.pkl -l danq_13.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 1 -lr 0.01 -wn 10.0 -ne 12 -mf danq_05.pkl -l danq_14.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 2 -lr 0.0001 -wn 10.0 -ne 12 -mf danq_06.pkl -l danq_15.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 2 -lr 0.002 -wn 10.0 -ne 12 -mf danq_07.pkl -l danq_16.log &&
-python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 2 -lr 0.01 -wn 10.0 -ne 12 -mf danq_08.pkl -l danq_17.log # -halt
+python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 0 -lr 0.0001 -ne 12 -mf danq_00.pkl -l danq_00.log &&
+python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 0 -lr 0.002 -ne 12 -mf danq_01.pkl -l danq_01.log &&
+python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 0 -lr 0.01 -ne 12 -mf danq_02.pkl -l danq_02.log &&
+python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 1 -lr 0.0001 -ne 12 -mf danq_03.pkl -l danq_03.log &&
+python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 1 -lr 0.002 -ne 12 -mf danq_04.pkl -l danq_04.log &&
+python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 1 -lr 0.01 -ne 12 -mf danq_05.pkl -l danq_05.log &&
+python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 2 -lr 0.0001 -ne 12 -mf danq_06.pkl -l danq_06.log &&
+python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 2 -lr 0.002 -ne 12 -mf danq_07.pkl -l danq_07.log &&
+python baseline_train.py -d /n/data_02/Basset/data/encode_roadmap.h5 -mt 4 -optim 2 -lr 0.01 -ne 12 -mf danq_08.pkl -l danq_08.log # -halt
