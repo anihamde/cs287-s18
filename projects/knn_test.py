e5d78e891599b6578da2671756718dbdf21e075f
elbertgong <elbertgong@college.harvard.edu>
2018-05-02 23:04:00 -0400

    traindex error

--- projects/knn_test.py
+++ projects/knn_test.py
@@ -21,7 +21,7 @@ from baseline_model import *
 parser = argparse.ArgumentParser(description='training runner')
 parser.add_argument('--data','-d',type=str,default='/n/data_02/Basset/data/roadmap/histone/histone_token.h5',help='path to training data')
 parser.add_argument('--model_type','-mt',type=int,default=3,help='Model type')
-# parser.add_argument('--batch_size','-bs',type=int,default=128,help='Batch size')
+parser.add_argument('--batch_size','-bs',type=int,default=128,help='Batch size')
 parser.add_argument('--num_epochs','-ne',type=int,default=10,help='Number of epochs')
 parser.add_argument('--model_file','-mf',type=str,default='/n/data_01/cs287-s18/projects/knn_00.pkl',help='Load model filename')
 parser.add_argument('--stop_instance','-halt',action='store_true',help='Stop AWS instance after training run.')
