1b4d383b71ab25d704eca55a47ad63a714339db7
sjgosai <sgosai@g.harvard.edu>
2018-05-07 13:18:43 -0400

    Starting to write

--- projects/model_maker.py
+++ projects/model_maker.py
@@ -1,1 +1,13 @@
 import torch
+import torch.nn as nn 
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch.utils.data import Dataset
+import numpy as np
+import h5py
+
+class model_maker:
+    def __init__(self,in_chn,in_width):
+        self.in_chn   = in_chn
+        self.in_width = in_width
+        self.Modules = []
