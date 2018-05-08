42bf31f8a7257d5ee9b15319b07c8dc6f8ffcb17
sjgosai <sgosai@g.harvard.edu>
2018-05-07 23:06:24 -0400

    added args.gene_drop_level default

--- projects/concat_train.py
+++ projects/concat_train.py
@@ -34,7 +34,7 @@ parser.add_argument('--model_file','-mf',type=str,default='stupid.pkl',help='Sav
 parser.add_argument('--stop_instance','-halt',action='store_true',help='Stop AWS instance after training run.')
 parser.add_argument('--log_file','-l',type=str,default='stderr',help='training log file')
 parser.add_argument('--workers', '-wk', type=int, help='number of data loading workers', default=2)
-parser.add_argument('--gene_drop_level','-gdl',type=int,help='0 indicates dropout on genes, 1 indicates dropout on the embedding.')
+parser.add_argument('--gene_drop_level','-gdl',type=int,default=1,help='0 indicates dropout on genes, 1 indicates dropout on the embedding.')
 args = parser.parse_args()
 
 print("Begin run")
