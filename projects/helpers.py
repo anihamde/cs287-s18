327319002944aee85d71166c8ff2100ea7d91d51
sjgosai <sgosai@g.harvard.edu>
2018-05-09 13:23:16 -0400

    Applying sigmoid to y_score

--- projects/helpers.py
+++ projects/helpers.py
@@ -2,6 +2,7 @@ import torch
 import time
 import math
 import torch
+import torch.nn.functional as F
 from torch.autograd import Variable
 from sklearn.metrics import roc_curve, auc, precision_recall_curve
 
@@ -67,6 +68,7 @@ def freeze_model(model):
 
 def calc_auc(model, y_test, y_score, auctype = "ROC"):
     if auctype == "ROC":
+        y_score = F.sigmoid(y_score)
         n_classes = y_test.shape[1] # 164
         fpr = dict()
         tpr = dict()
