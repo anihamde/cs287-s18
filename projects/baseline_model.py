f2210ca7c443d6b8350c38585bb23b678f313274
sjgosai <sgosai@g.harvard.edu>
2018-05-09 16:03:22 -0400

    Rollback to b4a1a9d

--- projects/baseline_model.py
+++ projects/baseline_model.py
@@ -203,7 +203,7 @@ class BassetNormCat(nn.Module):
                 module_list[-1].weight_g.data.clamp_(min=0.0,max=value)
             elif key.split('.')[-1] == 'weight' and len(module_list[-1].weight.data.size()) > 1:
                 module_list[-1].weight.data.renorm_(p=2,dim=0,maxnorm=value)
-                
+
 class BassetNormCat2(nn.Module):
     def __init__(self, dropout_prob=0.3, gene_drop_lvl=1):
         super(BassetNormCat2, self).__init__()
@@ -215,10 +215,10 @@ class BassetNormCat2(nn.Module):
         self.maxpool_4 = nn.MaxPool1d(4,padding=0)
         self.maxpool_3 = nn.MaxPool1d(3,padding=0)
         self.genelinear = LinearNorm(19795, 500, weight_norm=False)
-        self.linear1 = LinearNorm(200*13, 1000, weight_norm=False)
-        self.linear2 = LinearNorm(1000+500, 2000, weight_norm=False)
+        self.linear1 = LinearNorm(200*13+500, 1000, weight_norm=False)
+        self.linear2 = LinearNorm(1000, 1000, weight_norm=False)
         self.dropout = nn.Dropout(p=dropout_prob)
-        self.output = nn.Linear(2000, 1)
+        self.output = nn.Linear(1000, 1)
         self.gdl = gene_drop_lvl
     def forward(self, x, geneexpr):
         out1 = F.relu(self.conv1(x)) # 3 of these
@@ -233,8 +233,6 @@ class BassetNormCat2(nn.Module):
         out = F.pad(out,(1,1))
         out = self.maxpool_4(out) # (?, 500, 8)
         out = out.view(-1, 200*13) # (?, 500*8)
-        out = F.relu(self.linear1(out)) # (?, 800)
-        out = self.dropout(out)
         if self.gdl == 0:
             geneexpr = self.dropout(geneexpr)
             geneexpr = F.relu(self.genelinear(geneexpr))
@@ -242,6 +240,8 @@ class BassetNormCat2(nn.Module):
             geneexpr = F.relu(self.genelinear(geneexpr)) # (?, 500)
             geneexpr = self.dropout(geneexpr)
         out = torch.cat([out, geneexpr], dim=1) # (?, 200*13+500)
+        out = F.relu(self.linear1(out)) # (?, 800)
+        out = self.dropout(out)
         out = F.relu(self.linear2(out)) # (?, 800)
         out = self.dropout(out)
         return self.output(out) # (?, 1)
@@ -255,7 +255,52 @@ class BassetNormCat2(nn.Module):
                 module_list[-1].weight_g.data.clamp_(min=0.0,max=value)
             elif key.split('.')[-1] == 'weight' and len(module_list[-1].weight.data.size()) > 1:
                 module_list[-1].weight.data.renorm_(p=2,dim=0,maxnorm=value)
-
+                
+class BassetNormCat_bilin(nn.Module):
+    def __init__(self, dropout_prob=0.3):
+        super(BassetNormCat_bilin, self).__init__()
+        self.conv1 = Conv1dNorm(4, 300, 19, stride=1, padding=0, weight_norm=False)
+        self.conv2 = Conv1dNorm(300, 200, 11, stride=1, padding=0, weight_norm=False)
+        self.conv3 = Conv1dNorm(200, 200, 7, stride=1, padding=0, weight_norm=False)
+        self.maxpool_4 = nn.MaxPool1d(4,padding=0)
+        self.maxpool_3 = nn.MaxPool1d(3,padding=0)
+        self.bilin = nn.Bilinear(200*13,19795,200*13+500)
+        self.linear1 = LinearNorm(200*13+500, 1000, weight_norm=False)
+        self.linear2 = LinearNorm(1000, 1000, weight_norm=False)
+        self.dropout = nn.Dropout(p=dropout_prob)
+        self.output = nn.Linear(1000, 1)
+    def forward(self, x, geneexpr):
+        #if sparse_in: # (?, 600, 4)
+        #    in_seq = to_one_hot(x, n_dims=4).permute(0,3,1,2).squeeze()
+        #else:
+        #    in_seq = x.squeeze()
+        x = F.pad(x,(9,9))
+        out = F.relu(self.conv1(x)) # (?, 4, 580)
+        out = self.maxpool_3(out) # (?, 30, 145)
+        out = F.pad(out,(5,5))
+        out = F.relu(self.conv2(out)) # (?, 300, 140)
+        out = self.maxpool_4(out) # (?, 300, 35)
+        out = F.pad(out,(3,3))
+        out = F.relu(self.conv3(out)) # (?, 500, 32)
+        out = F.pad(out,(1,1))
+        out = self.maxpool_4(out) # (?, 500, 8)
+        out = out.view(-1, 200*13) # (?, 500*8)
+        out = self.bilin(out,geneexpr) # (?,200*13+500)
+        out = F.relu(self.linear1(out)) # (?, 800)
+        out = self.dropout(out)
+        out = F.relu(self.linear2(out)) # (?, 800)
+        out = self.dropout(out)
+        return self.output(out) # (?, 1)
+    def clip_norms(self, value):
+        key_chain = [ key for key in self.state_dict().keys() if 'weight' in key ]
+        for key in key_chain:
+            module_list = [self]
+            for key_level in key.split('.')[:-1]:
+                module_list.append( getattr(module_list[-1], key_level) )
+            if key.split('.')[-1] == 'weight_g':
+                module_list[-1].weight_g.data.clamp_(min=0.0,max=value)
+            elif key.split('.')[-1] == 'weight' and len(module_list[-1].weight.data.size()) > 1:
+                module_list[-1].weight.data.renorm_(p=2,dim=0,maxnorm=value)
                 
 class Basset(nn.Module):
     def __init__(self, dropout_prob=0.3, output_labels=164):
@@ -496,4 +541,4 @@ class DanQCat_attn(nn.Module):
 #         out = out.transpose(1,0).reshape(-1,45*320) # (/, 45*320)
 
         out = F.relu(self.linear(out)) # (?, 925)
-        return self.output(out) # (?, 1)
+return self.output(out) # (?, 1)
