ecd966f0ed0af821ac0a6b68ad7693dcbc797c46
Anirudh Suresh <anirudhtx@gmail.com>
2018-05-14 13:26:53 -0500

    finish adding jaspar

--- projects/baseline_model.py
+++ projects/baseline_model.py
@@ -326,15 +326,10 @@ class DanQ(nn.Module):
         
         conv_weights = self.conv1.weight
         
-        print(conv_weights.size())
-
         JASPAR_motifs = list(np.load('JASPAR_CORE_2016_vertebrates.npy', encoding = 'latin1'))
 
         reverse_motifs = [JASPAR_motifs[19][::-1,::-1], JASPAR_motifs[97][::-1,::-1], JASPAR_motifs[98][::-1,::-1], JASPAR_motifs[99][::-1,::-1], JASPAR_motifs[100][::-1,::-1], JASPAR_motifs[101][::-1,::-1]]
         JASPAR_motifs = JASPAR_motifs + reverse_motifs
-        print(len(JASPAR_motifs))
-        print(len(JASPAR_motifs[0]))
-        print(len(JASPAR_motifs[0][0]))
 
         for i in range(len(JASPAR_motifs)):
             m = JASPAR_motifs[i][::-1,:]
